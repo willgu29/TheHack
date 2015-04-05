d0b1299a2c39831a675375024d2a364e2bd60109
Will Gu <willgu29@gmail.com>
2015-04-04 20:06:07 -0700

    Fuck

--- TheHack/FindShadowsViewController.m
+++ TheHack/FindShadowsViewController.m
@@ -128,6 +128,26 @@
     return 160;
 }
 
+
+-(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Remove seperator inset
+    if ([cell respondsToSelector:@selector(setSeparatorInset:)]) {
+        [cell setSeparatorInset:UIEdgeInsetsZero];
+    }
+    
+    // Prevent the cell from inheriting the Table View's margin settings
+    if ([cell respondsToSelector:@selector(setPreservesSuperviewLayoutMargins:)]) {
+        [cell setPreservesSuperviewLayoutMargins:NO];
+    }
+    
+    // Explictly set your cell's layout margins
+    if ([cell respondsToSelector:@selector(setLayoutMargins:)]) {
+        [cell setLayoutMargins:UIEdgeInsetsZero];
+    }
+    
+}
+
 #pragma mark - MDCSwipeToChooseDelegate Callbacks
 
 // This is called when a user didn't fully swipe left or right.
